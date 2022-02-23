0c1ec34976e5247e3969373db8fdfe5ca129cc7c
JL <55872611+jinnliu@users.noreply.github.com>
2022-02-23 16:04:30 +0800

    Add files via upload

--- bz2toh5.py
+++ bz2toh5.py
@@ -1,5 +1,5 @@
-"""   YPL, JLL, 2021.3.22 - 2022.2.16
-bz2toh5.py generates training data from rlog.bz2 for train_model.py (e.g. modelB5)
+"""   YPL, JLL, 2021.3.22 - 2022.2.22
+bz2toh5.py generates training data from rlog.bz2 for train_model5.py
 from /home/jinn/YPN/ypdesk/remove.py
 read /home/jinn/YPN/yp-Efficient1/bz2toh5_plot.py
 
@@ -29,9 +29,8 @@ def mkpath(all_dirs):
       #---  frame_positions.shape = (1200, 3)
       #---  frame_orientations.shape = (1200, 4)
       #---  frame_velocities.shape = (1200, 3)
-      #---  len(velocities) = 1200
+      #---  len(frame_times) = 1200
     num_f = len(frame_times)
-      #---  num_f = 1200
     if not os.path.isfile(dir + 'pathdata.h5'):
       with h5py.File(dir + 'pathdata.h5', 'w') as f:
         f.create_dataset('Path', (num_f-50, 51))
@@ -40,26 +39,24 @@ def mkpath(all_dirs):
           ecef_from_local = orient.rot_from_quat(frame_orientations[ori])
             #---  ecef_from_local.shape = (3, 3)
           local_from_ecef = ecef_from_local.T
-            #---  local_from_ecef.shape = (3, 3)
           frame_positions_local = np.einsum('ij,kj->ki', local_from_ecef, frame_positions[ori:] - frame_positions[ori])
             #---  frame_positions_local.shape = (1200, 3)
             #---  frame_positions_local.shape = (1199, 3)
-          print('#---  len(frame_positions_local) =', len(frame_positions_local))
           x_f = frame_positions_local[:50, 0]
           y_f = frame_positions_local[:50, 1]
-          linear_model = np.polyfit(x_f, y_f, 3)
-          linear_model_fn = np.poly1d(linear_model)
+          coeff = np.polyfit(x_f, y_f, 3)
+          cubic_fn = np.poly1d(coeff)
           vaild_len = x_f[-1]
-          x_e = np.array([1+i for i in range(50)])
-          y_e = linear_model_fn(x_e)
-          out = np.hstack([y_e, vaild_len])
-          f['Path'][ori] = out
+          x_p = np.array([1+i for i in range(50)])
+          y_p = cubic_fn(x_p)
+          path = np.hstack([y_p, vaild_len])
+          f['Path'][ori] = path
           f['V'][ori] = velocities[ori]
-          print(ori, out)
+          print(ori, path)
     fh5 = h5py.File(dir + 'pathdata.h5', 'r')   # read pathdata.h5
     Path = fh5['Path']
     Velo = fh5['V']
-    print('#---  shape.Path, shape.Velo =', shape.Path, shape.Velo)
+      #---  Path.shape, Velo.shape = (1149, 51) (1149, 1)
 
 def mkradar(files):
   for f in files:
@@ -112,7 +109,6 @@ def mkradar(files):
     dataset = fh5['LeadOne']
       #--- dataset.shape = (10, 5)  # 10 = 1201 - 1191 (10 frames)
       #--- dataset.dtype = float32
-      #print("    #--- dataset.dtype =", dataset.dtype)
 
 if __name__ == '__main__':
   dirs1 = os.listdir('/home/jinn/dataB')
@@ -144,14 +140,11 @@ print(new_list)
 print([l.which() for l in logs[:10]])
 ['initData', 'sentinel', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState']
 
-  print("#---  all_files =", all_files)
 #---  all_files = [
 '/home/jinn/dataA/8bfda98c9c9e4291|2020-05-11--03-00-57--61/camera.h5',
 '/home/jinn/dataA/8bfda98c9c9e4291|2020-05-11--03-00-57--61/radar.h5',
-'/home/jinn/dataA/8bfda98c9c9e4291|2020-05-11--03-00-57--61/rlog.bz2',
-'/home/jinn/dataA/8bfda98c9c9e4291|2020-05-11--03-00-57--61/fcamera.hevc']
+...]
 
-  print("#---  all_dirs =", all_dirs)
 #---  all_dirs = [
 '/home/jinn/dataB/UHD--2018-08-02--08-34-47--33/',
 '/home/jinn/dataB/UHD--2018-08-02--08-34-47--32/']
