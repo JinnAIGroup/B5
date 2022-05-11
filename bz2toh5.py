b126b85579850320fa47dc318d7afa29b238b387
JL <55872611+jinnliu@users.noreply.github.com>
2022-05-11 16:25:38 +0800

    Add files via upload

--- bz2toh5.py
+++ bz2toh5.py
@@ -1,4 +1,4 @@
-"""   YPL, JLL, 2021.3.22 - 2022.4.29
+"""   YPL, JLL, 2021.3.22 - 2022.5.5
 bz2toh5.py generates net_outputs.lead (58-vector in outputs[3]) from rlog.bz2 for train_modelB5YJ.py
 from /home/jinn/openpilot/tools/lib/bz2toh5B5.py
 read /home/jinn/YPN/yp-Efficient1/bz2toh5_plot.py
@@ -20,29 +20,43 @@ def mkradar(files):
     if 'bz2' not in f:
       continue
     print('#---  f =', f)
-    lr = LogReader(f)  # <= ents <= event_read_multiple_bytes() <= capnp_log <= log <= log.capnp
+    lr = LogReader(f)  # <= ents <= event_read_multiple_bytes() <= capnp_log <= log = capnp.load()
       #---  lr = <tools.lib.logreader.LogReader object at 0x7f8bf0406c10>
     logs = list(lr)
+    print('#---  logs[0] =', logs[0])
+    print('#---  logs[100] =', logs[100])
     new_list = [l.which() for l in logs]
-    new_list = list(set(new_list))   # get unique items (to set and back to list)
+    new_list = list(set(new_list))   # set() creates a set of unique items in Python
     print('#---  len(logs), len(new_list) =', len(logs), len(new_list))
       #---  len(logs), len(new_list) = 76267 28
 
     CS_t = np.array([l.logMonoTime*10**-9 for l in logs if l.which()=='carState'])
     RS_t = np.array([l.logMonoTime*10**-9 for l in logs if l.which()=='radarState'])
     print('#---  len(CS_t), len(RS_t) =', len(CS_t), len(RS_t))
-      #---  len(CS_t), len(RS_t) = 6000 1200
+      #---  len(CS_t), len(RS_t) = 6000 1200  (100 Hz, 20 Hz or FPS)
 
     plt.plot([l.carState.vEgo for l in logs if l.which() == 'carState'], linewidth=3)  # carState, vEgo defined in log.capnp
-    plt.title('Car speed from raw logs (m/s)', fontsize=25)
-    plt.xlabel('boot time (s)', fontsize=18)
+    plt.title('Car speed from raw logs', fontsize=25)
+    plt.xlabel('video time (10 ms)', fontsize=18)
     plt.ylabel('speed (m/s)', fontsize=18)
     plt.show()
 
     plt.plot([l.radarState.leadOne.dRel for l in logs if l.which() == 'radarState'], linewidth=3)
     plt.title('leadOne.dRel', fontsize=25)
-    plt.xlabel('frame no.', fontsize=18)
+    plt.xlabel('frame no. (20 FPS)', fontsize=18)
     plt.ylabel('dRel (m)', fontsize=18)
+    plt.show()
+
+    plt.plot([l.radarState.leadOne.yRel for l in logs if l.which() == 'radarState'], linewidth=3)
+    plt.title('leadOne.yRel', fontsize=25)
+    plt.xlabel('frame no. (20 FPS)', fontsize=18)
+    plt.ylabel('yRel (m)', fontsize=18)
+    plt.show()
+
+    plt.plot([l.radarState.leadOne.modelProb for l in logs if l.which() == 'radarState'], linewidth=3)
+    plt.title('leadOne.modelProb', fontsize=25)
+    plt.xlabel('frame no. (20 FPS)', fontsize=18)
+    plt.ylabel('Probability', fontsize=18)
     plt.show()
 
     plt.plot([l.radarState.leadTwo.dRel for l in logs if l.which() == 'radarState'], linewidth=3)
