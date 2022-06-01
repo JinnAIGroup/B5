0850d07d2e2c89e6cb94735621d8cacf391b7ddf
JL <55872611+jinnliu@users.noreply.github.com>
2022-06-01 12:39:05 +0800

    Add files via upload

--- bz2toh5.py
+++ bz2toh5.py
@@ -1,13 +1,14 @@
-"""   YPL, JLL, 2021.3.22 - 2022.5.5
-bz2toh5.py generates net_outputs.lead (58-vector in outputs[3]) from rlog.bz2 for train_modelB5YJ.py
+"""   YPL, JLL, 2021.3.22 - 2022.6.1
+for (Lead)  radarState.leadOne; radarState.leadTwo from raw_log.bz2 by supercombo.keras
+    (Track) to generate 58-vector (5 clusters) in outputs[3]
+bz2toh5.py generates net_outputs.lead (58-vector in outputs[3]) for train_modelB5YJ.py
 from /home/jinn/openpilot/tools/lib/bz2toh5B5.py
 read /home/jinn/YPN/yp-Efficient1/bz2toh5_plot.py
 
 (OP082) jinn@Liu:~/openpilot/tools/lib$ python bz2toh5.py
 Input:
-  /home/jinn/dataB/UHD--2018-08-02--08-34-47--32/raw_log.bz2 (/dataB USA)
+  /home/jinn/dataB/.../raw_log.bz2  (USA data)
 Output:
-  /home/jinn/dataB/UHD--2018-08-02--08-34-47--32/lead_data.h5
 """
 import os
 import h5py
@@ -15,7 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tools.lib.logreader import LogReader
 
-def mkradar(files):
+def mkradarTrack(files):
   for f in files:
     if 'bz2' not in f:
       continue
@@ -35,34 +36,60 @@ def mkradar(files):
     print('#---  len(CS_t), len(RS_t) =', len(CS_t), len(RS_t))
       #---  len(CS_t), len(RS_t) = 6000 1200  (100 Hz, 20 Hz or FPS)
 
+def mkradarLead(files):
+  for f in files:
+    if 'bz2' not in f:
+      continue
+    print('#---  f =', f)
+    lr = LogReader(f)  # <= ents <= event_read_multiple_bytes() <= capnp_log <= log = capnp.load()
+      #---  lr = <tools.lib.logreader.LogReader object at 0x7f8bf0406c10>
+    logs = list(lr)
+    print('#---  logs[0] =', logs[0])
+    print('#---  logs[100] =', logs[100])
+    new_list = [l.which() for l in logs]
+    new_list = list(set(new_list))   # set() creates a set of unique items in Python
+    print('#---  len(logs), len(new_list) =', len(logs), len(new_list))
+      #---  len(logs), len(new_list) = 76267 28
+
+    CS_t = np.array([l.logMonoTime*10**-9 for l in logs if l.which()=='carState'])
+    RS_t = np.array([l.logMonoTime*10**-9 for l in logs if l.which()=='radarState'])
+    print('#---  len(CS_t), len(RS_t) =', len(CS_t), len(RS_t))
+      #---  len(CS_t), len(RS_t) = 6000 1200  (100 Hz, 20 Hz or FPS)
+
+    plt.figure()
+    plt.subplot(2, 3, 1)
     plt.plot([l.carState.vEgo for l in logs if l.which() == 'carState'], linewidth=3)  # carState, vEgo defined in log.capnp
-    plt.title('Car speed from raw logs', fontsize=25)
+      #y = [l.carState.vEgo for l in logs if l.which() == 'carState']
+      #---  len(y) = 6000
+    plt.title('Car speed from raw logs', fontsize=20)
     plt.xlabel('video time (10 ms)', fontsize=18)
     plt.ylabel('speed (m/s)', fontsize=18)
-    plt.show()
 
+    plt.subplot(2, 3, 2)
     plt.plot([l.radarState.leadOne.dRel for l in logs if l.which() == 'radarState'], linewidth=3)
-    plt.title('leadOne.dRel', fontsize=25)
-    plt.xlabel('frame no. (20 FPS)', fontsize=18)
+    plt.title('leadOne.dRel', fontsize=20)
+      #plt.xlabel('frame no. (20 FPS)', fontsize=18)
     plt.ylabel('dRel (m)', fontsize=18)
-    plt.show()
 
+    plt.subplot(2, 3, 3)
     plt.plot([l.radarState.leadOne.yRel for l in logs if l.which() == 'radarState'], linewidth=3)
-    plt.title('leadOne.yRel', fontsize=25)
-    plt.xlabel('frame no. (20 FPS)', fontsize=18)
+    plt.title('leadOne.yRel', fontsize=20)
+      #plt.xlabel('frame no. (20 FPS)', fontsize=18)
     plt.ylabel('yRel (m)', fontsize=18)
-    plt.show()
 
-    plt.plot([l.radarState.leadOne.modelProb for l in logs if l.which() == 'radarState'], linewidth=3)
-    plt.title('leadOne.modelProb', fontsize=25)
-    plt.xlabel('frame no. (20 FPS)', fontsize=18)
-    plt.ylabel('Probability', fontsize=18)
-    plt.show()
+      #plt.subplot(2, 3, 4)
 
+    plt.subplot(2, 3, 5)
     plt.plot([l.radarState.leadTwo.dRel for l in logs if l.which() == 'radarState'], linewidth=3)
-    plt.title('leadTwo.dRel', fontsize=25)
+    plt.title('leadTwo.dRel', fontsize=20)
     plt.xlabel('frame no.', fontsize=18)
     plt.ylabel('dRel (m)', fontsize=18)
+
+    plt.subplot(2, 3, 6)
+    plt.plot([l.radarState.leadTwo.yRel for l in logs if l.which() == 'radarState'], linewidth=3)
+    plt.title('leadTwo.yRel', fontsize=20)
+    plt.xlabel('frame no. (20 FPS)', fontsize=18)
+    plt.ylabel('yRel (m)', fontsize=18)
     plt.show()
 
     leadOne = [l.radarState.leadOne for l in logs if l.which()=='radarState']
@@ -70,7 +97,7 @@ def mkradar(files):
     print('#---  len(leadOne), len(leadTwo) =', len(leadOne), len(leadTwo))
 
       #lead_file = f.replace('rlog.bz2', 'lead_data.h5')   # use /dataA (Taiwan)
-    lead_file = f.replace('raw_rlog.bz2', 'lead_data.h5')   # use /dataB (USA)
+    '''lead_file = f.replace('raw_rlog.bz2', 'lead_data.h5')   # use /dataB (USA)
     frames = len(leadOne)  # total frames
     lead = []
     if not os.path.isfile(lead_file):   # if lead_data.h5 does not exist
@@ -92,20 +119,21 @@ def mkradar(files):
       #--- list(fh5.keys()) = ['LeadOne']
     #dataset = fh5['LeadOne']
       #--- dataset.shape = (10, 5)  # 10 = 1201 - 1191 (10 frames)
-      #--- dataset.dtype = float32''''''
+      #--- dataset.dtype = float32'''
 
 if __name__ == '__main__':
   dirs = os.listdir('/home/jinn/dataB')
   dirs = ['/home/jinn/dataB/'+ i +'/' for i in dirs]
-  all_files = []
+  all_files = []#
   for di1 in dirs:
     dir1 = os.listdir(di1)
     files = [di1 + f for f in dir1]
     for f in files:
       all_files.append(f)
-  mkradar(all_files)
-
+  mkradarLead(all_files)
+  #mkradarTrack(all_files)
 '''
+  /home/jinn/dataB/.../lead_data.h5 (L)
 #---  len(logs) = 69061
 #---  len(new_list)  = 37
 print(new_list)
@@ -119,4 +147,10 @@ print(new_list)
 
 print([l.which() for l in logs[:10]])
 ['initData', 'sentinel', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState', 'roadCameraState']
+
+    plt.plot([l.radarState.leadOne.modelProb for l in logs if l.which() == 'radarState'], linewidth=3)
+    plt.title('leadOne.modelProb', fontsize=25)
+    plt.xlabel('frame no. (20 FPS)', fontsize=18)
+    plt.ylabel('Probability', fontsize=18)
+    plt.show()
 '''
